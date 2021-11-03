6a58f246896eee6e1c04ec309071acc111ab53d8
unaguna <k.izumi.ysk@gmail.com>
2021-11-03 19:51:43 +0900

    ✅Test for creating jsonschema of varchar (with unlimited length)

--- src/test/test__column2jsonschema.py
+++ src/test/test__column2jsonschema.py
@@ -13,7 +13,7 @@ TABLE = os.path.basename(os.path.splitext(__file__)[0])
 
 class TestColumn2Jsonschema:
 
-    def test__varchar(self):
+    def test__varchar_n(self):
         """ Normal test.
         """
         length = 123
@@ -43,3 +43,33 @@ class TestColumn2Jsonschema:
 
         # schema.maxLength equals to length of varchar
         assert jsonschema['maxLength'] == length
+
+    def test__varchar(self):
+        """ Normal test.
+        """
+        col_name = 'col'
+        tbl_query = f"CREATE TABLE {SCHEMA}.{TABLE}({col_name} varchar)"
+        col_query = f"SELECT * FROM information_schema.columns WHERE table_schema='{SCHEMA}' AND " \
+                    f"table_name='{TABLE}' AND column_name='{col_name}'"
+
+        with psycopg2.connect(DSN) as conn, conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
+            cur.execute(tbl_query)
+            cur.execute(col_query)
+
+            column = cur.fetchone()
+            pprint(dict(column))
+
+            jsonschema = col2jsonschema(column)
+            conn.rollback()
+
+        pprint(jsonschema)
+
+        ######################################################################
+        # Assertion
+        ######################################################################
+        assert jsonschema.keys() == {'type'}
+
+        # schema.type contains 'string'.
+        # If the column is nullable, schema.type contains 'null'.
+        assert isinstance(jsonschema['type'], list)
+        assert set(jsonschema['type']) == {'string', 'null'}
